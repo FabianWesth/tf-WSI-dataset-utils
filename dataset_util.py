8dd6efc56a03facd97c239347ff979ff839e41f4
Fabian Westhaeusser <fabian.westhaeusser@zmnh.uni-hamburg.de>
2021-12-21 13:10:35 +0100

    changes

--- dataset_util.py
+++ dataset_util.py
@@ -7,71 +7,104 @@ import pandas as pd
 from PIL import ImageFilter, Image
 import matplotlib.pyplot as plt
 from skimage.transform import PiecewiseAffineTransform, warp
-from skimage.color import rgb2hsv,hsv2rgb,rgb2lab,lab2rgb
+from skimage.color import rgb2hsv, hsv2rgb, rgb2lab, lab2rgb
 from skimage import exposure
 
+
 def save_wsi_thumbnail_mask(filename):
-    '''
+    """
     saves a low resolution png mask of the tissue location in a WSI
 
-    '''
+    """
 
-    try: filename = filename.numpy()
-    except: filename = filename
+    try:
+        filename = filename.numpy()
+    except:
+        filename = filename
+
+    print(filename)
     wsi = openslide.OpenSlide(filename)
 
+    # try:
+    #     print(f"Trying 3: {filename.decode()}")
+    #     wsi = openslide.OpenSlide(filename.decode())
+    #     print(f"Success 3: {filename.decode()}")
+    # except:
+    #     print(f"Trying 4: {filename}")
+    #     wsi = openslide.OpenSlide(filename)
+    #     print(f"Success 4: {filename}")
+
     def find_tissue_mask():
-        thumbnail = wsi.get_thumbnail((thumbnail_size,thumbnail_size))
+        thumbnail = wsi.get_thumbnail((thumbnail_size, thumbnail_size))
         thumbnail_blurred = np.array(thumbnail.filter(ImageFilter.GaussianBlur(radius=10)))
-        ret2,mask = cv2.threshold(thumbnail_blurred[:,:,0],0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        kernel = np.ones((5,5),np.uint8)
-        mask = cv2.erode(mask,kernel,iterations = 1)
-        mask[mask==0] = 1
-        mask[mask==255] = 0
+        ret2, mask = cv2.threshold(thumbnail_blurred[:, :, 0], 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        kernel = np.ones((5, 5), np.uint8)
+        mask = cv2.erode(mask, kernel, iterations=1)
+        mask[mask == 0] = 1
+        mask[mask == 255] = 0
         return mask
 
     l_dims = wsi.level_dimensions
-    thumbnail_size = min(2000., max(l_dims[0]))
+    thumbnail_size = min(2000.0, max(l_dims[0]))
 
-    mask_path = '{}_MASK.png'.format(filename.split('.')[0])
+    try:
+        mask_path = "{}_MASK.png".format(filename.decode().split(".")[0])
+    except:
+        mask_path = "{}_MASK.png".format(filename.split(".")[0])
 
     if not os.path.isfile(mask_path):
         print(filename)
-        mask = find_tissue_mask()*255
+        mask = find_tissue_mask() * 255
         mask_PIL = Image.fromarray(mask)
         mask_PIL.save(mask_path)
 
+
 def get_random_wsi_patch(filename, patch_size=256, downsample=4, augment=0):
-    '''
+    """
     takes a wsi and returns a random patch of patch_size
     downsample must be a multiple of 2
     run save_wsi_thumbnail_mask() before using this function
     augment = [0,1] the percent of data that will be augmented
 
-    '''
-    try: augment = augment.numpy()
-    except: augment = augment
-    try: patch_size = patch_size.numpy()
-    except: patch_size = patch_size
-    try: filename = filename.numpy()
-    except: filename = filename
-    try: downsample = downsample.numpy()
-    except: downsample = downsample
+    """
+    try:
+        augment = augment.numpy()
+    except:
+        augment = augment
+    try:
+        patch_size = patch_size.numpy()
+    except:
+        patch_size = patch_size
+    try:
+        filename = filename.numpy()
+    except:
+        filename = filename
+    try:
+        downsample = downsample.numpy()
+    except:
+        downsample = downsample
 
     if augment > 0:
         # pad for affine
-        patch_size = patch_size+4
+        patch_size = patch_size + 4
 
-    wsi = openslide.OpenSlide(filename)
+    try:
+        print(f"Trying 1: {filename.decode()}")
+        wsi = openslide.OpenSlide(filename.decode())
+        print(f"Success 1: {filename.decode()}")
+    except:
+        print(f"Trying 2: {filename}")
+        wsi = openslide.OpenSlide(filename)
+        print(f"Success 2: {filename}")
 
     l_dims = wsi.level_dimensions
     level = wsi.get_best_level_for_downsample(downsample + 0.1)
 
     # get or create wsi mask
     try:
-        mask_path = '{}_MASK.png'.format(filename.decode().split('.')[0])
+        mask_path = "{}_MASK.png".format(filename.decode().split(".")[0])
     except:
-        mask_path = '{}_MASK.png'.format(filename.split('.')[0])
+        mask_path = "{}_MASK.png".format(filename.split(".")[0])
     if not os.path.isfile(mask_path):
         save_wsi_thumbnail_mask(filename)
     mask = np.array(Image.open(mask_path))
@@ -86,46 +119,50 @@ def get_random_wsi_patch(filename, patch_size=256, downsample=4, augment=0):
 
         while True:
 
-            if level == -1: # if no resolution works return white region
-                print('{} broken | using white patch...'.format(filename))
-                return np.ones((patch_size, patch_size,3))*255
+            if level == -1:  # if no resolution works return white region
+                print("{} broken | using white patch...".format(filename))
+                return np.ones((patch_size, patch_size, 3)) * 255
 
             try:
                 level_dims = l_dims[level]
                 level_downsample = wsi.level_downsamples[level]
                 thumbnail_size = float(max(mask.shape))
-                mask_scale = thumbnail_size/max(level_dims)
+                mask_scale = thumbnail_size / max(level_dims)
                 scale_factor = int(round(downsample / level_downsample))
             except:
-                print('{} broken | using white patch...'.format(filename))
-                return np.ones((patch_size, patch_size,3))*255
+                print("{} broken | using white patch...".format(filename))
+                return np.ones((patch_size, patch_size, 3)) * 255
 
             try:
                 # select random pixel with tissue
-                idx = random.choice(np.argwhere(mask_vec==255))[0]
+                idx = random.choice(np.argwhere(mask_vec == 255))[0]
                 x_mask = x_ind[idx]
                 y_mask = y_ind[idx]
 
                 # calc wsi patch start indicies
-                patch_width = patch_size*scale_factor
-                x_start = int( (x_mask / mask_scale) - (patch_width/2))
-                y_start = int( (y_mask / mask_scale) - (patch_width/2))
+                patch_width = patch_size * scale_factor
+                x_start = int((x_mask / mask_scale) - (patch_width / 2))
+                y_start = int((y_mask / mask_scale) - (patch_width / 2))
 
-                region = wsi.read_region((int(x_start*level_downsample),int(y_start*level_downsample)), level, (patch_width,patch_width))
+                region = wsi.read_region(
+                    (int(x_start * level_downsample), int(y_start * level_downsample)),
+                    level,
+                    (patch_width, patch_width),
+                )
 
                 if scale_factor > 1:
                     region = region.resize((patch_size, patch_size), resample=1)
-                region = np.array(region)[:,:,:3]
+                region = np.array(region)[:, :, :3]
 
-                def colorshift(img, hbound=0.025, lbound=0.015): #Shift Hue of HSV space and Lightness of LAB space
-                    hShift=np.random.normal(0,hbound)
-                    lShift=np.random.normal(1,lbound)
-                    img=rgb2hsv(img)
-                    img[:,:,0]=(img[:,:,0]+hShift)
-                    img=hsv2rgb(img)
-                    img=rgb2lab(img)
-                    img[:,:,0]=exposure.adjust_gamma(img[:,:,0],lShift)
-                    img=lab2rgb(img)
+                def colorshift(img, hbound=0.025, lbound=0.015):  # Shift Hue of HSV space and Lightness of LAB space
+                    hShift = np.random.normal(0, hbound)
+                    lShift = np.random.normal(1, lbound)
+                    img = rgb2hsv(img)
+                    img[:, :, 0] = img[:, :, 0] + hShift
+                    img = hsv2rgb(img)
+                    img = rgb2lab(img)
+                    img[:, :, 0] = exposure.adjust_gamma(img[:, :, 0], lShift)
+                    img = lab2rgb(img)
                     return img
 
                 def PiecewiseAffine(img, points=8):
@@ -138,9 +175,13 @@ def get_random_wsi_patch(filename, patch_size=256, downsample=4, augment=0):
                     # add offset
                     dst_rows = np.zeros(src[:, 1].shape) + src[:, 1]
                     for i in list(range(points))[1:-1]:
-                        dst_rows[i::points] += np.random.normal(loc=0, scale=rows/(points*10), size=dst_rows[i::points].shape)
+                        dst_rows[i::points] += np.random.normal(
+                            loc=0, scale=rows / (points * 10), size=dst_rows[i::points].shape
+                        )
                     dst_cols = np.zeros(src[:, 0].shape) + src[:, 0]
-                    dst_cols[points:-points] += np.random.normal(loc=0,scale=rows/(points*10), size=dst_cols[points:-points].shape)
+                    dst_cols[points:-points] += np.random.normal(
+                        loc=0, scale=rows / (points * 10), size=dst_cols[points:-points].shape
+                    )
                     dst = np.vstack([dst_cols, dst_rows]).T
                     # compute transform
                     tform = PiecewiseAffineTransform()
@@ -151,33 +192,34 @@ def get_random_wsi_patch(filename, patch_size=256, downsample=4, augment=0):
 
                 if np.random.random() < augment:
                     # augment image
-                    region = (region/255.).astype(np.float64)
+                    region = (region / 255.0).astype(np.float64)
                     region = colorshift(region)
                     region = PiecewiseAffine(region)
-                    region = np.uint8(region*255.)
+                    region = np.uint8(region * 255.0)
                     # unpad
-                    region = region[2:-2,2:-2,:]
+                    region = region[2:-2, 2:-2, :]
 
                 return region
 
             except:
-                print('{} broken for level {}'.format(filename,level))
+                print("{} broken for level {}".format(filename, level))
                 level -= 1
-                print('\ttrying level {}'.format(level))
+                print("\ttrying level {}".format(level))
 
     region = get_random_patch(wsi, l_dims, level, mask, patch_size, filename, downsample, augment)
     # region = np.transpose(region, (2,0,1)) # [CWH]
     return region
+
 
 def get_slide_label(filename, data_label_xlsx):
     data_label_xlsx = str(data_label_xlsx.numpy())
     filename = str(filename.numpy())
     # get slide label
     df = pd.read_excel(data_label_xlsx)
-    name = filename.split('/')[-1]
-    index = df.index[df['wsi']==name].tolist()
+    name = filename.split("/")[-1]
+    index = df.index[df["wsi"] == name].tolist()
     if index == []:
         label = np.array([-1])
     else:
-        label = np.array(df['class'][index])
+        label = np.array(df["class"][index])
     return label
--- example_usage.py
+++ example_usage.py
@@ -1,35 +1,65 @@
 import tensorflow as tf
-from dataset_util import get_random_wsi_patch
+import glob
+import os
+from os import listdir
+from os.path import isfile, join
+import random
+from dataset_util import get_random_wsi_patch, save_wsi_thumbnail_mask
 
-def input_setup(batch_size=32, dataset_dir='./path_to_folder_with_WSIs', wsi_ext='.svs'):
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-    '''
+
+def get_immediate_files(mypath):
+    return [f for f in listdir(mypath) if isfile(join(mypath, f)) and f.endswith(".svs")]
+
+
+def input_setup(
+    batch_size=32, dataset_dir="/home/fabian/UKE_projects/CPC/data/AIH_DILI/liver_cleaned/WSI/AIH/", wsi_ext=".svs"
+):
+
+    """
     This function sets up the input data pipeline
     takes input at tfrecord files
-    '''
+    """
 
     #######################################################################
 
     # data A input pipeline
-    wsi_paths = list(glob('{}*{}'.format(dataset_dir, wsi_ext)))
-    wsi_paths = [str(path) for path in wsi_paths]
+    # wsi_paths = list(glob('{}*{}'.format(dataset_dir, wsi_ext)))
+    # wsi_paths = [str(path) for path in wsi_paths]
+
+    wsi_paths = get_immediate_files(dataset_dir)
+    wsi_paths = [join(dataset_dir, path) for path in wsi_paths]
+
+    for path in wsi_paths:
+        save_wsi_thumbnail_mask(path)
 
     random.shuffle(wsi_paths)
     image_count = len(wsi_paths)
-    print('found: {} images from WSI dataset'.format(image_count))
+    print("found: {} images from WSI dataset".format(image_count))
 
     # setup tf dataset using py_function
     path_ds = tf.data.Dataset.from_tensor_slices(wsi_paths)
     path_ds = path_ds.repeat(100)
-    wsi_dataset = path_ds.map(lambda filename: tf.py_function(get_random_wsi_patch, [filename], tf.float32), num_parallel_calls=40)
+
+    def get_random_wsi_patch_lambda(filename):
+        return tf.py_function(get_random_wsi_patch, [filename], tf.float32)
+
+    wsi_dataset = path_ds.map(lambda filename: get_random_wsi_patch_lambda(filename), num_parallel_calls=40)
+    # wsi_dataset = path_ds.map(tf.py_function(get_random_wsi_patch_lambda, tf.float32), num_parallel_calls=40)
     wsi_dataset = wsi_dataset.shuffle(100)
     wsi_dataset = wsi_dataset.batch(batch_size=batch_size, drop_remainder=True)
-    wsi_dataset = wsi_dataset.prefetch(buffer_size=1) # <-- very important for efficency
-    iterator = tf.data.Iterator.from_structure(wsi_dataset.output_types, wsi_dataset.output_shapes)
-    training_init_op = iterator.make_initializer(wsi_dataset)
-    get_input = iterator.get_next()
+    wsi_dataset = wsi_dataset.prefetch(buffer_size=1)  # <-- very important for efficency
+    # iterator = tf.data.Iterator.from_structure(wsi_dataset.output_types, wsi_dataset.output_shapes)
+    # training_init_op = iterator.make_initializer(wsi_dataset)
+    # get_input = iterator.get_next()
+    for wsi in wsi_dataset:
+        print(wsi)
 
-    '''
+    # wsi_dataset_iter = iter(wsi_dataset)
+    # pos_batch = wsi_dataset_iter.get_next()
+
+    """
     to use run:
         "sess.run([training_init_op])"
 
@@ -39,4 +69,8 @@ def input_setup(batch_size=32, dataset_dir='./path_to_folder_with_WSIs', wsi_ext
     for more info see:
             https://www.tensorflow.org/versions/r1.15/api_docs/python/tf/data/Dataset
 
-    '''
+    """
+
+
+if __name__ == "__main__":
+    input_setup()
